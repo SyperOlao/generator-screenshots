223e722a7adb824917d9d410cc88176af4d395cc
SyperOlao <olay1234538@gmail.com>
2024-02-22 02:19:36 +0300

    add generation by for

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import asyncio
 from generate_screenshot.vk.generate_vk import GenerateScreenshotVk
+import time
 
-url = "https://vk.com/styd.pozor?w=wall-71729358_13128057"
-# url = "https://vk.com/instasamka?w=wall-182863061_923576"
-# url = "https://vk.com/instasamka?w=wall-182863061_925645"
-# url = "https://vk.com/al_feed.php?w=wall-90802042_128826"
-# url = "https://vk.com/istoriyadonbassa?w=wall-189776399_233201 "
-# url = "https://vk.com/public179350392?w=wall-179350392_9"
-# url = "https://vk.com/istoriyadonbassa?w=wall-189776399_233200"
-# url = "https://vk.com/delphine_chik?w=wall515103149_134"
-url = "https://vk.com/@-194707757-rss-1610825261-1959186610#"
-#url = "https://vk.com/wall-141359201_1489951"
-# url = "https://vk.com/wall-93250065_1198328"
-
-
+urls = [
+    "https://vk.com/public222510135?w=wall-222510135_41",
+    "https://vk.com/styd.pozor?w=wall-71729358_13128057",
+# "https://vk.com/instasamka?w=wall-182863061_923576",
+# "https://vk.com/instasamka?w=wall-182863061_925645",
+# "https://vk.com/al_feed.php?w=wall-90802042_128826",
+# "https://vk.com/istoriyadonbassa?w=wall-189776399_233201",
+# "https://vk.com/public179350392?w=wall-179350392_9",
+# "https://vk.com/istoriyadonbassa?w=wall-189776399_233200",
+# "https://vk.com/delphine_chik?w=wall515103149_134",
+# "https://vk.com/@-194707757-rss-1610825261-1959186610#",
+# "https://vk.com/wall-141359201_1489951",
+# "https://vk.com/wall-93250065_1198328",
+# "https://vk.com/wall-224359326_12"
+]
 
 
 # wl_replies_block_wrap
 async def main():
-   vk = GenerateScreenshotVk()
-   await vk.generate_screen_shot(url, "test2.png")
+    vk = GenerateScreenshotVk()
+    for url in urls:
+        start_time = time.time()
+        name = url.split('/')[-1]
+        try:
+            await vk.generate_screen_shot(url, f"screenshots/{name}.png")
+        except Exception as e:
+            print(f"Screenshot {name} does not exists, {e}")
+        end_time = time.time()
+        print(f"Время выполнения: {end_time - start_time} секунд для {name}")
 
 
 asyncio.run(main())
