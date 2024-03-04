7b8444b241e1a23ead6f0355fdc3035d4aa75394
SyperOlao <olay1234538@gmail.com>
2024-03-04 17:59:07 +0300

    try to copy slides

--- copy_pptx/copy_pptx.py
+++ copy_pptx/copy_pptx.py
@@ -5,15 +5,17 @@ import zipfile
 import shutil
 import os
 
+
 warnings.filterwarnings("ignore", category=UserWarning)
 
 script_location = Path(__file__).absolute().parent
 
 
-async def copy_pptx():
+def copy_pptx():
     new_presentation = Presentation()
     path_to_new = f"{script_location}/res.pptx"
     new_presentation.save(path_to_new)
+
     path_to_source = f"{script_location}/template.pptx"
     source_presentation = Presentation(path_to_source)
 
@@ -23,33 +25,37 @@ async def copy_pptx():
 
 def copy_slides(source_pptx, target_pptx, slides_to_copy):
     # Открываем исходный PPTX файл как ZIP-архив
+    source_folder = f"{script_location}/source_pptx_extracted"
+    os.makedirs(source_folder, exist_ok=True)
+
     with zipfile.ZipFile(source_pptx, 'r') as source_zip:
         # Извлекаем содержимое исходного PPTX файла
-        source_zip.extractall('source_pptx_extracted')
+        source_zip.extractall(source_folder)
 
     # Открываем целевой PPTX файл как ZIP-архив
     with zipfile.ZipFile(target_pptx, "a") as target_zip:
         for slide_num in slides_to_copy:
             # Формируем путь к XML-файлу слайда в исходном PPTX файле
-            slide_xml_path = f"source_pptx_extracted/ppt/slides/slide{slide_num}.xml"
+            slide_xml_path = f"{source_folder}/ppt/slides/slide{slide_num}.xml"
             # Добавляем XML-файл слайда в целевой PPTX файл
             target_zip.write(slide_xml_path, f"ppt/slides/slide{slide_num}.xml")
 
-            # Копирование медиафайлов, связанных с слайдом
-            media_path = f"source_pptx_extracted/ppt/media/"
+            # Копирование медиафайлов, связанных с текущим слайдом
+            media_path = f"{source_folder}/ppt/media/"
             for file in os.listdir(media_path):
-                source_file = os.path.join(media_path, file)
-                target_file = f"ppt/media/{file}"
-                if os.path.exists(source_file):
-                    target_zip.write(source_file, target_file)
+                if file.startswith(f"slide{slide_num}"):
+                    target_zip.write(os.path.join(media_path, file), f"ppt/media/{file}")
 
-            # Копирование замечаний к слайду
-            notes_path = f"source_pptx_extracted/ppt/notesSlides/"
+            # Копирование замечаний к текущему слайду
+            notes_path = f"{source_folder}/ppt/notesSlides/"
             for file in os.listdir(notes_path):
-                source_file = os.path.join(notes_path, file)
-                target_file = f"ppt/notesSlides/{file}"
-                if os.path.exists(source_file):
-                    target_zip.write(source_file, target_file)
+                if file.startswith(f"slide{slide_num}"):
+                    target_zip.write(os.path.join(notes_path, file), f"ppt/notesSlides/{file}")
+
+        # Добавляем остальные необходимые файлы из исходной презентации
+        for file in source_zip.namelist():
+            if file.startswith("ppt/") and file != "ppt/slides/_rels":
+                target_zip.write(os.path.join(source_folder, file), file)
 
     # Удаляем временную папку с извлеченным содержимым исходного PPTX файла
-    shutil.rmtree('source_pptx_extracted')
+    shutil.rmtree(source_folder)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 from copy_pptx.copy_pptx import copy_pptx
 
-asyncio.run(copy_pptx())
+copy_pptx()
