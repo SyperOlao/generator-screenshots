d32863aea08d577ae27c0b7234cc34d261b6d919
SyperOlao <olay1234538@gmail.com>
2024-03-10 15:08:27 +0300

    coping pptx

--- copy_pptx/copy_pptx.py
+++ copy_pptx/copy_pptx.py
@@ -23,7 +23,7 @@ def copy_pptx():
     path_to_source = f"{script_location}/template.pptx"
     source_presentation = Presentation(path_to_source)
 
-    copy_slides(path_to_source, path_to_new, [1, 2])
+    copy_slides(path_to_source, path_to_new, [1, 2, 4, 7, 27, 21])
     # new_presentation.save(path_to_new)
 
 
@@ -61,69 +61,35 @@ def copy_slides(source_pptx, target_pptx, slides_to_copy):
                     and not file.startswith("ppt/slideLayouts"):
                 target_zip.write(os.path.join(source_folder, file), file)
 
+        copy_all_files(source_zip, target_zip, source_folder)
     # shutil.rmtree(source_folder)
-
-    # for file in source_zip.namelist():
-    #     if file.startswith("ppt/") \
-    #             and not file.startswith("ppt/notesSlides") \
-    #             and not file.startswith("ppt/slides") \
-    #             and not file.startswith("ppt/slideLayouts"):
-    #         target_zip.write(os.path.join(source_folder, file), file)
 
 
 def working_with_xml(source_folder, slides_to_copy):
     root_pptx_xml = f"{source_folder}/ppt/presentation.xml"
 
-    tree = ET.parse(root_pptx_xml)
+    tree = etree.parse(root_pptx_xml)
 
     root = tree.getroot()
-    # for child in root:
-    #     print(f"Tag: {child.tag}, Attr {child.attrib}")
-
-    # Загрузка XML-документа из файла
-    tree2 = etree.parse(root_pptx_xml)
-
-    # Получение корневого элемента
-    root2 = tree2.getroot()
-
+    temp_list = [x + 2 for x in slides_to_copy]
     my_namespaces = dict([
         node for _, node in ET.iterparse(StringIO(str(ET.tostring(root), encoding='utf-8')), events=['start-ns'])])
     pprint(my_namespaces)
-    elements = root2.xpath('//ns0:sldIdLst', namespaces=my_namespaces)
-    elems = root2.xpath('//ns0:sldId', namespaces=my_namespaces)
+    sld_id_lst = root.xpath('//ns0:sldIdLst', namespaces=my_namespaces)
+    sld_ids = root.xpath('//ns0:sldId', namespaces=my_namespaces)
     to_remove = []
-    for elem in elems:
-        r_id = elem.get('{http://schemas.openxmlformats.org/officeDocument/2006/relationships}id')
+    for sldId in sld_ids:
+        r_id = sldId.get('{http://schemas.openxmlformats.org/officeDocument/2006/relationships}id')
         r_num = int(r_id.replace('rId', ''))
-        if r_num not in slides_to_copy:
-            to_remove.append(elem)
+        if r_num not in temp_list:
+            to_remove.append(sldId)
 
     for sldId in to_remove:
         parent = sldId.getparent()
         if parent is not None:
             parent.remove(sldId)
 
-    # for elem in elements:
-    #     print(elem)
-    # elems = root.findall('.//sldId', namespaces=my_namespaces)
-    # # Нахождение элемента для удаления по атрибуту r:id
-    # to_remove = []
-    #
-    # for sldId in elems:
-    #     r_id = sldId.get('{http://schemas.openxmlformats.org/officeDocument/2006/relationships}id')
-    #     r_num = int(r_id.replace('rId', ''))
-    #     if r_num not in slides_to_copy:
-    #         to_remove.append(sldId)
-
-    # for sldId in to_remove:
-    #     parent = sldId.getparent()
-    #     elems.remove(sldId)
-    #     root.remove(sldId)
-
-    # Сохранение обновлённого XML-документа
-
-    tree2.write(f"{source_folder}/ppt/presentation2.xml", pretty_print=True, xml_declaration=True, encoding='utf-8')
-    tree2.write(root_pptx_xml)
+    tree.write(root_pptx_xml, pretty_print=True, xml_declaration=True, encoding='utf-8')
 
 
 def copy_solely_necessary_files(source_zip, target_zip, source_folder):
